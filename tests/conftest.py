f171b780a61f68d448789d0f43435649feba0dc8
ibis <toni@apeworx.io>
2022-12-13 14:45:51 -0600

    Attempt creating networks/providers with arbitrary chain IDs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
 from contextlib import contextmanager
 from itertools import chain
+from tempfile import mkdtemp
 from typing import List
 
 import pytest
 
-from ape.api import NetworkAPI, ProviderContextManager
+from ape.api import NetworkAPI, ProviderContextManager, create_network_type
 from ape.types import LogFilter
 from ape_test.provider import LocalProvider
 
@@ -14,9 +15,32 @@ class Bridge:
     def __init__(self, *network_names: str):
         self._providers = {}
 
+        chain_id = 1
+        network_id = 1
         for network_name in network_names:
-            network = NetworkAPI.create_adhoc_network()
-            network.name = network_name
+            ethereum_class = None
+            for plugin_name, ecosystem_class in NetworkAPI.plugin_manager.ecosystems:
+                if plugin_name == "ethereum":
+                    ethereum_class = ecosystem_class
+                    break
+
+            if ethereum_class is None:
+                raise Exception("Core Ethereum plugin missing.")
+
+            data_folder = mkdtemp()
+            request_header = NetworkAPI.config_manager.REQUEST_HEADER
+
+            network_cls = create_network_type(chain_id, network_id)
+            network = network_cls(
+                name=network_name,
+                ecosystem=ethereum_class(
+                    data_folder=data_folder,
+                    request_header=request_header,
+                ),
+                data_folder=data_folder,
+                request_header=request_header,
+                _default_provider="test",
+            )
 
             provider = LocalProvider(
                 name="test",
@@ -85,8 +109,8 @@ def owner(accounts):
 
 
 @pytest.fixture(scope="session")
-def bridge(networks):
-    return Bridge(networks, "testnet1", "testnet2")
+def bridge():
+    return Bridge("testnet1", "testnet2")
 
 
 @pytest.fixture(scope="session")
