562bd8d59ce2f6eb7825dd32f6a341c030978cd6
ibis <toni@apeworx.io>
2023-02-15 22:49:19 -0600

    Add documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,44 @@ import pytest
 
 from .utils import ConnextBridge
 
-
+# For now our ConnextBridge is responsible for creating the networks and
+# providers used in cross-chain calls. The prototype implementation accepts
+# name strings, which determine the number and names of networks to create.
+#
+# In the future the user will be able to provide their own network/provider, so
+# that they can utilize live testnet versions of Connext.
 NETWORK_PING = "network_ping"
 NETWORK_PONG = "network_pong"
 
 
 @pytest.fixture(scope="session")
 def owner(accounts):
+    """
+    The default test account to use for Connext, Ping, and Pong deployments.
+    """
     return accounts[0]
 
 
 @pytest.fixture(scope="session")
 def bridge(owner):
+    """
+    The bridge that stands up two local networks for Connext testing.
+
+    During this constructor the bridge will create two HardhatProvider instances
+    and deploy its Connext.sol contract to each of them.
+    """
     return ConnextBridge(owner, NETWORK_PING, NETWORK_PONG)
 
 
 @pytest.fixture(scope="session")
 def contracts(project, owner, bridge):
+    """
+    The Ping and Pong contracts, deployed to their respective networks.
+
+    Because this example includes authentication, we must place both contracts
+    in this 'contracts' fixture so that we can call authenticate() appropriately
+    after the contracts deploy.
+    """
     with bridge.use_network(NETWORK_PING) as provider:
         ping = owner.deploy(project.Ping, bridge.connext.address, owner)
         bridge.register_contract(ping)
--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -2,6 +2,12 @@ from tests.conftest import NETWORK_PING, NETWORK_PONG
 
 
 def test_bridge(bridge, owner, ping, pong):
+    """
+    Tests nested cross-chain calls between the Ping and Pong contracts:
+        - User initiates ping-pong via Ping.sendPing()
+        - Bridge picks up Ping's xcall and relays it to Pong
+        - Pong makes its own xcall which the bridge relays back to Ping
+    """
     with bridge.use_network(NETWORK_PING):
         ping.sendPing(sender=owner)
 
